ac18769d960bde8f5a8e7ad299630a423e3007f4
pankona <yosuke.akatsuka@gmail.com>
2014-05-21 20:33:19 +0900

    add cookbook for libjpeg

---
+++ site-cookbooks/libjpeg/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: libjpeg
+# Recipe:: default
+#
+# Copyright 2014, pankona
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'libjpeg-turbo' do
+  action :install
+end
