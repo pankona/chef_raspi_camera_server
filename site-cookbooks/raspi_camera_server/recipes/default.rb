f342d3bee41c0ccd71fd62463df407d14d11d2f8
aYosukeAkatsuka <yosuke.akatsuka@access-company.com>
2014-05-23 19:52:22 +0900

    update raspi_camera_server's recipe - fix to deploy correctly

--- site-cookbooks/raspi_camera_server/recipes/default.rb
+++ site-cookbooks/raspi_camera_server/recipes/default.rb
@@ -7,16 +7,39 @@
 # All rights reserved - Do Not Redistribute
 #
 
-deploy_revision '/usr/local/raspi_camera_server' do
+deploy_revision '/home/aka/raspi_camera_server' do
+  user "aka"
+  group "aka"
   repository 'https://github.com/pankona/raspi_camera_server.git'
   migrate false
   environment "RAILS_ENV" => "development"
   action :deploy
-  before_restart do
-    bash "bundle install" do
-      cwd release_path
+  before_symlink do
+    bash "mkdir on shared" do
+      user "aka"
+      group "aka"
+      cwd "#{release_path}/../.."
       code <<-BASHCODE
-        bundle install --path=./vendor/bundle
+        mkdir -p ./shared/pids
+        mkdir -p ./shared/log
+      BASHCODE
+    end
+  end
+  before_restart do
+    template "#{release_path}/.env" do
+      owner "aka"
+      group "aka"
+      source ".env.erb"
+      action :create
+    end
+    bash "bundle install" do
+      user "aka"
+      group "aka"
+      cwd release_path
+      timeout 36000
+      code <<-BASHCODE
+        source /home/aka/.bash_profile
+        bundle install
       BASHCODE
     end
   end
